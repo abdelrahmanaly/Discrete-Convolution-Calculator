a4bd433d085bcdc4dd78b312fbecf68c0f93cd72
Aly Abdelrahman <abdelrahmanaly5@gmail.com>
2019-09-27 02:02:30 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Discrete-Convolution-Calculator
+
+This Project is inspired by Digital Signal Processing and The fundemental concept of filtering in Electrical engieering. It is a Visual Representation of Convolution done using Python in the Discrete Domain. This is a great tool to help you visualize more how filter works, and if you have any questions please feel free to add comments and i can make more functions for this program.
+
+## Getting Started
+In order to get this Project you will need to clone this repo on your computer using the GUI of github.
+
+### Pre-requisites
+You will need to have a couple of things installed before we can get started.
+This project uses external libraries which you can install through pip3, This Code is written in Python3 and will not complie in a Python 2 interperter.
+
+```
+You will need to install Numpy library, this library is like a package of wonderful and handy calculator tools that we need to use in order to do the discrete calculation of convolution.
+
+You will also need to install Matplotlib, this library is like a package to Help you plot easier using built in GUI functions that some smart person already figured out.
+```
+
+### Installing
+
+To install Numpy, open up termminal and write the following:
+```
+pip3 install numpy 
+```
+if it doesn't work try using Sudo before the command:
+```
+sudo pip3 install numpy
+``` 
+
+To install Matplotlib, write in terminal the following command:
+```
+pip3 install matplotlib 
+```
+if it doesn't work try using Sudo before the command:
+```
+sudo pip3 install matplotlib
+```
+### Built for
+This project was done and built for any struglling electrical students, I did this as a learning project, and i will try to keep updating it by adding more functions, feel free to fork it and work on it as well.
+
+## Acknwledgments
+* Thank you matplotlib
+* Thank you Numpy
+
+Thank you for reading <3
---
+++ convolution of Discrete systems.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plot
+
+# this is just for me to learn python's matplot and numpy frameworks
+def gentriangle(x):
+    array=[0]*(x) #creating an array of 0, with the size of x
+    # i will have to split the function to even and odd
+    
+    for i in range(0,int(x/2)+1):
+        array[i]=i
+        if i==0:
+            continue
+        else:
+            array[len(array)-i-1]=array[i]
+    array[len(array)-1]=0
+    return array
+ ## issues with the gen square wave, the code's repeating twice
+def gensquare(a,x):
+    array=[a]*x
+    array[0]=0
+    array[len(array)-1]=0
+    return array
+
+def discrete_convolution(a,b):
+    array=np.convolve(a,b)
+    return array
+
+#note to self this is the discrete time of the process
+choice=int(input("which graphs would you you like to see convoloted, Square * triangle(1),Triangle *Triangle(2),Square * Square(3)"))
+graph_length=int(input("how long is your graph moving for ? ")) #make sure to make this an integer function as this function only returns strings.
+
+if choice==1: #this function doesnt even work
+    a=int(input("please insert amplitude of sqaure?"))
+    plot.subplot(333)
+    plot.title("Square Function")
+    square_wave_array=gensquare(a,graph_length)
+    square_wave=plot.stem(square_wave_array)
+    plot.setp(square_wave)
+    plot.subplot(331)
+    plot.title("Triangular Function")
+    triangle_wave=plot.stem(gentriangle(graph_length+1))  ## fix crappy code
+    plot.setp(triangle_wave) ## fix crappy code
+    plot.subplot(313)
+    plot.title("Resulting convolution of both functions")
+    convolution1=plot.stem(discrete_convolution(gensquare(a,graph_length),gentriangle(graph_length))) #issues in this line
+    plot.setp(convolution1)
+    plot.show()
+elif choice==2: #fix the triangle titles
+    plot.subplot(333)
+    plot.title("First trianguler function")
+    triangle_wave=plot.stem(gentriangle(graph_length+1))
+    plot.setp(triangle_wave)
+    plot.subplot(331)
+    plot.title("Second trianguler function")
+    triangle_wave=plot.stem(gentriangle(graph_length+1))
+    plot.setp(triangle_wave)
+    plot.subplot(313)
+    plot.title("Resulting convolution for both functions")
+    convolution1=plot.stem(discrete_convolution(gentriangle(graph_length),gentriangle(graph_length)))
+    plot.setp(convolution1)
+    plot.show()
+else:
+    a=int(input("please insert amplitude of sqaure?"))
+    plot.subplot(333)
+    plot.title("First Square function")
+    square_wave=plot.stem(gensquare(a,graph_length))
+    plot.setp(square_wave)
+    plot.subplot(331)
+    plot.title("Second Square function")
+    square_wave=plot.stem(gensquare(a,graph_length))
+    plot.setp(square_wave)
+    plot.subplot(313)
+    plot.title("Resulting convolution for both functions")
+    square=gensquare(a,graph_length)
+    convolution1=plot.stem(discrete_convolution(square,square))
+    plot.setp(convolution1)
+    plot.show()
+
+
+
